235b2886454596f45c875353ccda512511a2bac7
root <root@DESKTOP-J5IFQ89.localdomain>
2018-08-18 02:43:15 +0300

    Content added, on-hover scenarios worked, slightly improvements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-$('.work-piece').hover(
-       function(){ $(this).addClass('workpiece-after'); console.log("privet");},
-       function(){ $(this).removeClass('workpiece-after'); }
-)
+// $('.work-piece').hover(
+//        function(){ $(this).addClass('on-hover'); console.log("privet");},
+//        function(){ $(this).removeClass('on-hover'); }
+// )
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+	});
+});
